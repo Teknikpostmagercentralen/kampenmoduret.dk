10b3837e8a88379090b651ed1395ac96bd77608e
Daniel Graungaard <danielgraungaard@gmail.com>
2025-07-12 18:12:51 +0200

    test envs deploy

--- src/lib/config/config-constants.ts
+++ src/lib/config/config-constants.ts
@@ -3,17 +3,13 @@ import {env} from '$env/dynamic/public';
 
 export class ConfigConstants {
     static getURL(): string {
-        // Prefer CF_PAGES_URL if available (Cloudflare's environment)
-        if (env.PUBLIC_CURRENT_APP_URL) {
-            return env.PUBLIC_CURRENT_APP_URL;
-        }
-
         // Fallback to PUBLIC_CURRENT_APP_URL (your custom env.example value)
         if (env.PUBLIC_CURRENT_APP_URL) {
             return env.PUBLIC_CURRENT_APP_URL;
         }
-
-        // Fallback to localhost if all else fails
-        return "http://localhost:5173";
+        
+        //If not net in env just use it from the browser window
+        const baseUrl = `${window.location.protocol}//${window.location.host}`;
+        return baseUrl
     }
 }
