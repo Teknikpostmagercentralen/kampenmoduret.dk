5fe29fbfd46b8dcf1bf389ef4b6c161d82a89128
Michael A. Pedersen <cra7y.com@gmail.com>
2025-08-04 22:17:32 +0200

    Reintrodce team dead status check in write task

--- src/lib/firebase/firebaseconnection.ts
+++ src/lib/firebase/firebaseconnection.ts
@@ -414,10 +414,9 @@ export class FirebaseConnection {
 			throw new AuthenticationError('User is not authenticated. Please log in.');
 		}
 
-		// TODO: reenable check?
-		// if(await this.isTeamDead(currentUser.uid)) {
-		//     throw new TeamIsDeadError("You cannot complete more tasks because you have run out of time")
-		// }
+		if(await this.isTeamDead(currentUser.uid)) {
+		     throw new TeamIsDeadError("You cannot complete more tasks because you have run out of time")
+		}
 
 		const db = getDatabase(app);
 		const taskSnapshot = await get(ref(db, `${FirebaseConstants.TASKS_ROOT}/${taskId}`));
